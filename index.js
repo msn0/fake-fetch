53df5c5dbc271ac4ccca0eb2f4b45325d6f221b4
Michał Jezierski <ambinanitelo@gmail.com>
2016-02-26 23:52:42 +0100

    new api, cleanups

--- index.js
+++ index.js
@@ -1,55 +1,16 @@
-//'use strict';
-//
-//var sinon = require('sinon');
-//
-//var getOptions = function () {
-//  return window.fetch.firstCall.args[1] || {};
-//};
-//
-//module.exports.install = function () {
-//  sinon.stub(window, 'fetch');
-//};
-//
-//module.exports.restore = function () {
-//  window.fetch.restore();
-//};
-//
-//module.exports.getMethod = function () {
-//  return getOptions().method || 'get';
-//};
-//
-//module.exports.getBody = function () {
-//  return getOptions().body || '';
-//};
-//
-//module.exports.getUrl = function () {
-//  return window.fetch.firstCall.args[0];
-//};
-//
-//module.exports.respondWith = function (data, options) {
-//  return window.fetch.returns(Promise.resolve(new Response(data, options)));
-//};
-//
-//module.exports.when = function (url, options) {
-//  return window.fetch.getCalls().find(function (call) {
-//    var url = call.args[0];
-//    var options = call.args[1];
-//  });
-//};
+let fetchCopy;
 
-
-var fetchCopy;
-
-module.exports.install = function () {
+export function install() {
   fetchCopy = window.fetch;
-  window.fetch =  function () {};
+  window.fetch = function () {
+  };
 }
 
-module.exports.restore = function () {
-  window.fetch =  fetchCopy;
+export function restore() {
+  window.fetch = fetchCopy;
 }
 
-module.exports.when = function (givenUrl) {
+export function when(givenUrl) {
   window.fetch = function (expectedUrl) {
     if (givenUrl === expectedUrl) {
       Promise.resolve();
@@ -57,9 +18,11 @@ module.exports.when = function (givenUrl) {
   }
 }
 
+class FakeFetch {
+  constructor() {
 
-
-
+  }
+}
 
 
 //fakeFetch.get('/test', {method: 'get'}).respondWith('{"foo":"bar"}');
